3ef8ac03f9f4979cba7ac05909c08e9741e2d84b
Miki <mikolajkmieciak@gmail.com>
2024-07-30 11:08:12 +0200

    moving object and some constants to new file

--- app/src/main/java/com/example/footballmanager/network/ApiServiceInstance.kt
+++ app/src/main/java/com/example/footballmanager/network/ApiServiceInstance.kt
@@ -1,4 +1,19 @@
 package com.example.footballmanager.network
 
-class ApiServiceInstance {
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object FootballApi {
+    private const val BASE_URL =
+        "https://v3.football.api-sports.io"
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+        .build()
+
+    val retrofitService: FootballApiService by lazy {
+        retrofit.create(FootballApiService::class.java)
+    }
 }
--- app/src/main/java/com/example/footballmanager/network/FootballApiService.kt
+++ app/src/main/java/com/example/footballmanager/network/FootballApiService.kt
@@ -1,34 +1,18 @@
 package com.example.footballmanager.network
 
 import com.example.footballmanager.BuildConfig
-import com.google.gson.GsonBuilder
 import retrofit2.Call
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Query
 
-private const val BASE_URL =
-    "https://v3.football.api-sports.io"
-
-private val retrofit = Retrofit.Builder()
-    .baseUrl(BASE_URL)
-    .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
-    .build()
-
 interface FootballApiService {
     @Headers(
         "x-rapidapi-host: ${BuildConfig.RAPIDAPI_HOST}",
-        "x-rapidapi-key: ${BuildConfig.RAPIDAPI_KEY}")
+        "x-rapidapi-key: ${BuildConfig.RAPIDAPI_KEY}"
+    )
     @GET("fixtures")
     fun getFixtures(
         @Query("last") last: Int //2 characters
-    ):Call<FixtureDataWrapper>
-}
-
-object FootballApi{
-    val retrofitService: FootballApiService by lazy {
-        retrofit.create(FootballApiService::class.java)
-    }
+    ): Call<FixtureDataWrapper>
 }
