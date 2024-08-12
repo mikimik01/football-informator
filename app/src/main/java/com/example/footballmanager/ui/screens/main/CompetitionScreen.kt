c834b9c6c4a2c7e688e00d06567703a51a4905c9
Miki <mikolajkmieciak@gmail.com>
2024-08-12 10:20:43 +0200

    Add test crush as a button in CompetitionScreen layout

--- app/src/main/java/com/example/footballmanager/ui/screens/main/CompetitionScreen.kt
+++ app/src/main/java/com/example/footballmanager/ui/screens/main/CompetitionScreen.kt
@@ -1,12 +1,19 @@
 package com.example.footballmanager.ui.screens.main
 
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun CompetitionScreen(){
     Text(text = "Competition")
+    TextButton(onClick = {
+        throw RuntimeException("Test Crush")
+    }) {
+        Text(text = "Crash Test")
+    }
 }
 
 @Preview
